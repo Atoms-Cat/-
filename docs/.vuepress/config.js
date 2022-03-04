ab8db0dce5cbc8b50558a11f86136e1c4087981a
HowellYang <th15817161961@gmail.com>
2022-03-04 11:37:02 +0800

    feat

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // 站点配置
+    lang: 'zh-CN',
+    title: 'atom`s cat notion',
+    description: 'notion',
+
+    // 主题和它的配置
+    theme: '@vuepress/theme-default',
+    themeConfig: {
+        // logo: 'https://vuejs.org/images/logo.png',
+    },
+}
